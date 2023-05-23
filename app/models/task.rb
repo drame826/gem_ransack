e65a62fe0cb5bc69f1127f87970ab9b8c2004327
drame826 <dianguineds04@gmail.com>
2023-05-23 21:32:18 +0000

    keep

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,11 +4,7 @@ class Task < ApplicationRecord
   validates :status, presence: true
   validates :deadline, presence: true
 
-  searchable_columns = %w[title description] # Remplacez ces colonnes par celles de votre modèle Task
-
-  def self.ransackable_attributes(auth_object = nil)
-    searchable_columns + _ransackers.keys
-  end
+  
   # validate :deadline, :must_start_from_today
 
   # def must_start_from_today
